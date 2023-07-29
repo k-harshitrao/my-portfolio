0a58d923521cc2293010bd680c40bdf51cd1a1f9
kharshit <k.harshitrao2002@gmail.com>
2023-07-29 19:52:01 +0530

    first commit

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+import LogoH from '../../../assets/images/logo-h.png'
+
+const Logo = () => {
+
+    return (
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoH} alt="H" /> 
+        </div>
+    )
+}
+
+export default Logo
